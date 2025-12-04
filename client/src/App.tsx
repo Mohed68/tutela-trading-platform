127037292a0555b771a0fc21849f72e58a9dbf4c
Mohamed Obeidalla <mohed68@gmail.com>
2025-12-05 00:36:19 +0400

    Reorganize routes for marketplace and commodities

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import LandingPage from "./pages/landing";
 import Dashboard from "./pages/dashboard";
-import Marketplace from "./pages/marketplace";
 import Contracts from "./pages/contracts";
 import Partners from "./pages/partners";
 import Verification from "./pages/verification";
@@ -11,6 +10,7 @@ import NotFound from "./pages/not-found";
 import OffersRedirect from "./pages/offers";
 import CommoditiesRedirect from "./pages/commodities";
 import { ROUTES } from "./config/routes";
+import Marketplace from "./pages/marketplace";
 
 export default function App() {
   return (
@@ -23,14 +23,17 @@ export default function App() {
 
           {/* Protected Routes */}
           <Route path={ROUTES.dashboard} element={<Dashboard />} />
-          <Route path={ROUTES.marketplace} element={<Marketplace />} />
+          <Route path={ROUTES.commodities} element={<Marketplace />} />
           <Route path={ROUTES.contracts} element={<Contracts />} />
           <Route path={ROUTES.partners} element={<Partners />} />
           <Route path={ROUTES.verification} element={<Verification />} />
 
+           {/* Legacy aliases */}
+          <Route path={ROUTES.marketplace} element={<CommoditiesRedirect />} />
+
            {/* Redirected legacy routes */}
           <Route path={ROUTES.offers} element={<OffersRedirect />} />
-          <Route path={ROUTES.commodities} element={<CommoditiesRedirect />} />
+          <Route path={ROUTES.notFound} element={<NotFound />} />
 
           {/* 404 */}
           <Route path="*" element={<NotFound />} />
